b152e87b594aa177344c9d4b1537ff2345b2f2a9
vishnudevsecops1 <vvrraw123@gmail.com>
2026-02-05 23:04:00 +0000

    updated variables script

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ PERSON1
 echo "$PERSON1:: Hi $PERSON2 How are you doing?"
 echo "$PERSON2:: Hello $PERSON1, I am thanks for asking. How are you doing?"
 echo "$PERSON1:: Doing Great, what are you learning currently?"
-echo "$PERSON2:: I am learning Shell 
+echo "$PERSON2:: I am learning Shell"
