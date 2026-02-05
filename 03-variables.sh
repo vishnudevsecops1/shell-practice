c95b1f564f45ca61715bd32b440659d5a73d6a76
vishnudevsecops1 <vvrraw123@gmail.com>
2026-02-05 22:55:52 +0000

    updated variables script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PERSON1=trump #no space valuse and =
+PERSON2=putin
+PERSON1
+echo "$PERSON1:: Hi $PERSON2 How are you doing?"
+echo "$PERSON2:: Hello $PERSON1, I am thanks for asking. How are you doing?"
+echo "$PERSON1:: Doing Great, what are you learning currently?"
+echo "$PERSON2:: I am learning Shell 
