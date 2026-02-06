6a8ccbdcb408c40c9f1bdaaab518501e46d4b8c2
vishnudevsecops1 <vvrraw123@gmail.com>
2026-02-06 19:05:16 +0000

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -6,8 +6,10 @@ NUMBER=$1
 # -lt -> less than
 # -eq -> equal
 # -ne -> not equal
-if [ $NUMBER -gt 25]; then
+if [ $NUMBER -gt 25 ]; then
    echo "Given number: $NUMBER is greater than 25"
+elif [ $NUMBER -eq 25]; then
+   echo "Given number: $NUMBER is equal to 25"
 else
    echo "Given number: $NUMBER is less than 25"
 fi
